b6c4bf46270906fe3e97213425bcb0cf0e443b39
jay-1799 <patelj1799@gmail.com>
2024-12-24 19:06:43 -0500

    refactored repository functions

--- backend/repository/uptime_logs.go
+++ backend/repository/uptime_logs.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -79,33 +78,3 @@ func FetchUptimeData(db *sql.DB) []models.ServiceStatus {
 // 		log.Printf("Failed to store service status: %v", err)
 // 	}
 // }
-
-func AddSubscriber(db *sql.DB, emailID string) error {
-	_, err := db.Exec(
-		"INSERT INTO subscribers (email_id) VALUES ($1)", emailID,
-	)
-	if err != nil {
-		log.Printf("Failed to add email id: %v", err)
-	}
-	return err
-}
-
-func AddSubscriberToken(db *sql.DB, emailID string, token string) error {
-	query := `
-	INSERT INTO subscribers (email, token,is_verified,created_at,updated_at) 
-	VALUES ($1, $2, false, NOW(), NOW())
-	ON CONFLICT (email) DO UPDATE SET token=$2, created_at=NOW();
-	`
-	_, err := db.Exec(query, emailID, token)
-	return err
-}
-
-func VerifySubscriber(db *sql.DB, token string) (string, error) {
-	query := `UPDATE subscribers SET is_verified = true, updated_at=NOW() WHERE token=$1 RETURNING email;`
-	var email string
-	err := db.QueryRow(query, token).Scan(&email)
-	if err == sql.ErrNoRows {
-		return "", errors.New("invalid or expired token")
-	}
-	return email, err
-}
